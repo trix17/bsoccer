955a2672fd9d1ece6dcdc0e2be0f383d9699f971
trix17 <laurentjean90@yahoo.fr>
2022-02-03 20:07:17 -0500

    Finish, simple guessing game

--- soccer.py
+++ soccer.py
@@ -1,8 +1,9 @@
 def best_teams(best):
     wrong_guesses = 0
-    s = ["|"'\n',  "|"'\n',  "|""\n",  "|"'\n', "|","__"," ""__" " " "__"]
+    s = ["|"'\n',  "|"'\n',  "|""\n",  "|"'\n', "|","  ___ ___ ___"]
 
     l_left = list(best) # create a list "m""a""n""u""t""d"
+    
     boards = ["_"]*len(best) #boards is the length of the team ex: ______
     win = False
     print("Welcome to the this simple game ")
@@ -10,35 +11,40 @@ def best_teams(best):
 
 
 
-    while wrong_guesses < len(s)) - 1: # youhave the length of s amont of guesses 
+    while wrong_guesses < len(s) - 1: # youhave the length of s amont of guesses 
         print("\n")
-        guess = input("Guess the first letter of the best team ")
+        guess = input("Guess a letter of the best team ")
         if guess in l_left:
             char_index = l_left.index(guess)
             boards[char_index] = guess
-            l_left[char_index = '$']
+            l_left[char_index] = '$'
 
 
+        else:
+            wrong_guesses += 1
+
+        print(''.join(boards)) #he join() method takes all items in an iterable and joins them into one string
+        print('\n'.join(s[0:wrong_guesses+1]))
+
+
+        if '_' not in boards:
+            print("You win, Game Over")
+            print("".join(boards))
+
+            win = True 
+            break 
+    
+    if not win:
+        
+        print("\n")
+        print('\n'.join(s))
+        
+        print("Sorry , you lost, the word was {}".format(best))
         
 
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
 best_teams("manutd")
+        
+
 
 
 
